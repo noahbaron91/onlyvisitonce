6c57bddbc03cc27bb6025e2ebfb48af090d9c4fd
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-10-08 18:03:36 -0700

    fix: subdomain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,8 @@ app.use((req, res, next) => {
     return;
   }
 
+  console.log(req.subdomains);
+
   // Check if the bypass subdomain is present
   if (req.subdomains.includes('bypass')) {
     next();
